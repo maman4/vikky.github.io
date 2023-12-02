2a79f3be3adacf8e8f25492924253164c43e1112
maman4 <mamanadifirmansyah@gmail.com>
2023-12-02 11:42:50 +0700

    first commit

---
+++ Anggota/41. Yunifa Dwi lestari/41. Yunifa Dwi lestari/Form1.Designer.vb
@@ -0,0 +1,266 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.cmbjenis = New System.Windows.Forms.ComboBox()
+        Me.cmbmerk = New System.Windows.Forms.ComboBox()
+        Me.txtharga = New System.Windows.Forms.TextBox()
+        Me.txtjumlah = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.txttotal = New System.Windows.Forms.TextBox()
+        Me.txtdiskon = New System.Windows.Forms.TextBox()
+        Me.txtbayar = New System.Windows.Forms.TextBox()
+        Me.bHitung = New System.Windows.Forms.Button()
+        Me.bHapus = New System.Windows.Forms.Button()
+        Me.bKeluar = New System.Windows.Forms.Button()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Modern No. 20", 27.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(256, 9)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(251, 38)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Harga Petfood"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(35, 72)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(101, 15)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Jenis Makanan"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(35, 115)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(39, 15)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Merk"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(35, 152)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(47, 15)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Harga"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(35, 194)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(52, 15)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Jumlah"
+        '
+        'cmbjenis
+        '
+        Me.cmbjenis.FormattingEnabled = True
+        Me.cmbjenis.Location = New System.Drawing.Point(169, 72)
+        Me.cmbjenis.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.cmbjenis.Name = "cmbjenis"
+        Me.cmbjenis.Size = New System.Drawing.Size(147, 23)
+        Me.cmbjenis.TabIndex = 5
+        '
+        'cmbmerk
+        '
+        Me.cmbmerk.FormattingEnabled = True
+        Me.cmbmerk.Location = New System.Drawing.Point(169, 112)
+        Me.cmbmerk.Name = "cmbmerk"
+        Me.cmbmerk.Size = New System.Drawing.Size(147, 23)
+        Me.cmbmerk.TabIndex = 6
+        '
+        'txtharga
+        '
+        Me.txtharga.Location = New System.Drawing.Point(169, 145)
+        Me.txtharga.Name = "txtharga"
+        Me.txtharga.Size = New System.Drawing.Size(147, 22)
+        Me.txtharga.TabIndex = 7
+        '
+        'txtjumlah
+        '
+        Me.txtjumlah.Location = New System.Drawing.Point(172, 187)
+        Me.txtjumlah.Name = "txtjumlah"
+        Me.txtjumlah.Size = New System.Drawing.Size(144, 22)
+        Me.txtjumlah.TabIndex = 8
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(426, 80)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(39, 15)
+        Me.Label6.TabIndex = 9
+        Me.Label6.Text = "Total"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(426, 120)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(51, 15)
+        Me.Label7.TabIndex = 10
+        Me.Label7.Text = "Diskon"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(426, 152)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(46, 15)
+        Me.Label8.TabIndex = 11
+        Me.Label8.Text = "Bayar"
+        '
+        'txttotal
+        '
+        Me.txttotal.Location = New System.Drawing.Point(506, 80)
+        Me.txttotal.Name = "txttotal"
+        Me.txttotal.Size = New System.Drawing.Size(165, 22)
+        Me.txttotal.TabIndex = 12
+        '
+        'txtdiskon
+        '
+        Me.txtdiskon.Location = New System.Drawing.Point(506, 120)
+        Me.txtdiskon.Name = "txtdiskon"
+        Me.txtdiskon.Size = New System.Drawing.Size(165, 22)
+        Me.txtdiskon.TabIndex = 13
+        '
+        'txtbayar
+        '
+        Me.txtbayar.Location = New System.Drawing.Point(506, 152)
+        Me.txtbayar.Name = "txtbayar"
+        Me.txtbayar.Size = New System.Drawing.Size(165, 22)
+        Me.txtbayar.TabIndex = 14
+        '
+        'bHitung
+        '
+        Me.bHitung.Location = New System.Drawing.Point(81, 229)
+        Me.bHitung.Name = "bHitung"
+        Me.bHitung.Size = New System.Drawing.Size(77, 32)
+        Me.bHitung.TabIndex = 15
+        Me.bHitung.Text = "Hitung"
+        Me.bHitung.UseVisualStyleBackColor = True
+        '
+        'bHapus
+        '
+        Me.bHapus.Location = New System.Drawing.Point(331, 229)
+        Me.bHapus.Name = "bHapus"
+        Me.bHapus.Size = New System.Drawing.Size(90, 32)
+        Me.bHapus.TabIndex = 16
+        Me.bHapus.Text = "Hapus"
+        Me.bHapus.UseVisualStyleBackColor = True
+        '
+        'bKeluar
+        '
+        Me.bKeluar.Location = New System.Drawing.Point(622, 227)
+        Me.bKeluar.Name = "bKeluar"
+        Me.bKeluar.Size = New System.Drawing.Size(70, 34)
+        Me.bKeluar.TabIndex = 17
+        Me.bKeluar.Text = "Keluar"
+        Me.bKeluar.UseVisualStyleBackColor = True
+        '
+        'ListView1
+        '
+        Me.ListView1.HideSelection = False
+        Me.ListView1.Location = New System.Drawing.Point(26, 283)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(725, 177)
+        Me.ListView1.TabIndex = 18
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(789, 519)
+        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.bKeluar)
+        Me.Controls.Add(Me.bHapus)
+        Me.Controls.Add(Me.bHitung)
+        Me.Controls.Add(Me.txtbayar)
+        Me.Controls.Add(Me.txtdiskon)
+        Me.Controls.Add(Me.txttotal)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.txtjumlah)
+        Me.Controls.Add(Me.txtharga)
+        Me.Controls.Add(Me.cmbmerk)
+        Me.Controls.Add(Me.cmbjenis)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("News701 BT", 9.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.Name = "Form1"
+        Me.Text = "                 "
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents cmbjenis As ComboBox
+    Friend WithEvents cmbmerk As ComboBox
+    Friend WithEvents txtharga As TextBox
+    Friend WithEvents txtjumlah As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents txttotal As TextBox
+    Friend WithEvents txtdiskon As TextBox
+    Friend WithEvents txtbayar As TextBox
+    Friend WithEvents bHitung As Button
+    Friend WithEvents bHapus As Button
+    Friend WithEvents bKeluar As Button
+    Friend WithEvents ListView1 As ListView
+End Class
---
+++ Anggota/41. Yunifa Dwi lestari/41. Yunifa Dwi lestari/Form1.vb
@@ -0,0 +1,140 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cmbjenis.Items.Add("Makanan Kucing")
+        cmbjenis.Items.Add("makanan Anjing")
+        cmbjenis.Items.Add("Makanan Burung")
+
+        Dim Arr(3, 1) As String
+        Arr(0, 0) = "Jenis Makanan Hewan"
+        Arr(0, 1) = "Merk"
+        Arr(1, 0) = "Harga"
+        Arr(1, 1) = "Jumlah"
+        Arr(2, 0) = "Total Harga"
+        Arr(2, 1) = "Diskon"
+        Arr(3, 0) = "Total Bayar"
+
+        ListView1.GridLines = True
+        ListView1.View = View.Details
+
+        For Baris = 0 To 3
+            For kolom = 0 To 1
+                ListView1.Columns.Add(Arr(Baris, kolom), 100)
+            Next Kolom
+        Next Baris
+    End Sub
+
+    Private Sub cmbjenis_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbjenis.SelectedIndexChanged
+        If cmbjenis.Text = "Makanan Kucing" Then
+            cmbmerk.Items.Clear()
+            cmbjenis.Text = ""
+            cmbmerk.Items.Add("Dry Food Coucou")
+            cmbmerk.Items.Add("Dry Food Whiskas")
+            cmbmerk.Items.Add("Felibite")
+            cmbmerk.Items.Add("Creamy Trats Me-0")
+
+        ElseIf cmbjenis.Text = "Makanan Anjing" Then
+            cmbmerk.Items.Clear()
+            cmbjenis.Text = ""
+            cmbmerk.Items.Add("Science Diet")
+            cmbmerk.Items.Add("Pedigree")
+            cmbmerk.Items.Add("Dog Choize")
+            cmbmerk.Items.Add("Rocco")
+
+        ElseIf cmbjenis.Text = "Makanan Burung" Then
+            cmbmerk.Items.Clear()
+            cmbjenis.Text = ""
+            cmbmerk.Items.Add("Vitakraft Amazonian")
+            cmbmerk.Items.Add("Health & Vitalyty Blend")
+            cmbmerk.Items.Add("wachsen Royal Canary")
+            cmbmerk.Items.Add("Prestige Loro Parque African")
+        End If
+    End Sub
+
+    Private Sub cmbmerk_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbmerk.SelectedIndexChanged
+        If cmbmerk.Text = "Dry Food Coucou" Then
+            txtharga.Text = 100000
+        ElseIf cmbmerk.Text = "Dry Food Whiskas" Then
+            txtharga.Text = 72000
+        ElseIf cmbmerk.Text = "Felibite" Then
+            txtharga.Text = 26000
+        ElseIf cmbmerk.Text = "Creamy Trats Me-0" Then
+            txtharga.Text = 25000
+        ElseIf cmbmerk.Text = "Science Diet" Then
+            txtharga.Text = 700000
+        ElseIf cmbmerk.Text = "pedigree" Then
+            txtharga.Text = 70000
+        ElseIf cmbmerk.Text = "DogChoize" Then
+            txtharga.Text = 60000
+        ElseIf cmbmerk.Text = "Dog Choize" Then
+            txtharga.Text = 20000
+        ElseIf cmbmerk.Text = "Rocco" Then
+            txtharga.Text = 55000
+        ElseIf cmbmerk.Text = "Vitakraft" Then
+            txtharga.Text = 100000
+        ElseIf cmbmerk.Text = "Health & Vitalyty Blend" Then
+            txtharga.Text = 150000
+        ElseIf cmbmerk.Text = "Wachsen Royal Canary" Then
+            txtharga.Text = 100000
+        ElseIf cmbmerk.Text = "Prestige Loro Parque Afican" Then
+            txtharga.Text = 150000
+        End If
+    End Sub
+
+    Private Sub bHitung_Click(sender As Object, e As EventArgs) Handles bHitung.Click
+        txttotal.Text = Val(txtharga.Text) * Val(txtjumlah.Text)
+        If txttotal.Text > 300000 Then
+            txtdiskon.Text = 15000
+        Else
+            txtdiskon.Text = 0
+        End If
+        txtbayar.Text = Val(txttotal.Text) - Val(txtdiskon.Text)
+
+        Dim Arr(6) As String
+        Arr(0) = cmbjenis.Text
+        Arr(1) = cmbmerk.Text
+        Arr(2) = txtharga.Text
+        Arr(3) = txtjumlah.Text
+        Arr(4) = txttotal.Text
+        Arr(5) = txtdiskon.Text
+        Arr(6) = txtbayar.Text
+
+        Dim listitem As ListViewItem
+        listitem = New ListViewItem
+        listitem = ListView1.Items.Add(Arr(0))
+        listitem.SubItems.Add(Arr(1))
+        listitem.SubItems.Add(Arr(2))
+        listitem.SubItems.Add(Arr(3))
+        listitem.SubItems.Add(Arr(4))
+        listitem.SubItems.Add(Arr(5))
+        listitem.SubItems.Add(Arr(6))
+        txtharga.Text = ""
+        txtjumlah.Text = ""
+    End Sub
+
+    Private Sub bHapus_Click(sender As Object, e As EventArgs) Handles bHapus.Click
+        If MsgBox("Silahakan Pilih 'Yes' untuk keluar dari from dan `No` untuk tetap difrom ini!!",
+                 MsgBoxStyle.YesNo, "pilihan!") = MsgBoxResult.Yes Then
+            Me.Close()
+        Else
+            MsgBox("Anda tetap di from ini", MsgBoxStyle.Information, "info")
+        End If
+
+    End Sub
+
+    Private Sub bKeluar_Click(sender As Object, e As EventArgs) Handles bKeluar.Click
+        If MsgBox("Silahkan Pilih `Yes` untuk menghapus data dan `No` untuk membatalkan!!!",
+                  MsgBoxStyle.YesNo, "Pilihan!") = MsgBoxResult.Yes Then
+            Me.
+            txtbayar.Text = ""
+            txtdiskon.Text = ""
+            txtharga.Text = ""
+            txtjumlah.Text = ""
+            txttotal.Text = ""
+            cmbjenis.Text = ""
+            cmbmerk.Text = ""
+            ListView1.Items.Clear()
+        Else MsgBox("Data Anda Masih Tersimpan", MsgBoxStyle.Information, "info!")
+
+        End If
+    End Sub
+End Class
